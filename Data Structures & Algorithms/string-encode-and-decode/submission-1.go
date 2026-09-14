c39d76300c637eabf021997ba7fcf920f1e19434
onesiphorus5 <138511256+onesiphorus5@users.noreply.github.com>
2026-09-13 17:45:58 -0700

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.go
@@ -0,0 +1,32 @@
+type Solution struct{}
+
+func (s *Solution) Encode(strs []string) string {
+    // output: for each string first add a byte denoting the number of bytes in the string
+    out := []byte{}
+    for _, str := range(strs) {
+        out = append(out, byte(len(str)))
+        out = append(out, []byte(str)...);
+    }
+
+    return string(out)
+}
+
+func (s *Solution) Decode(encoded string) []string {
+    original := []string{}
+    
+    curr_size := 0
+    for i := 0; i<len(encoded); {
+        curr_size = int(encoded[i])
+        i++ // updated at places where a byte is read
+
+        str := []byte{}
+        end := i + curr_size
+        for j := i; j < end; j++ {
+            str = append(str, encoded[j]);
+            i++ // updated at places where a byte is read
+        }
+        original = append(original, string(str));
+    }
+
+    return original
+}
